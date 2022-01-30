2db8324bf1e199e5f9b50035257f8124c320647e
Abhay Kumar <kumarabhay401@gmail.com>
2022-01-30 12:22:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+//dom method buttton and result
 let button = document.getElementById('btn');
 let result = document.getElementById('result');
 
@@ -5,10 +7,11 @@ let url = "https://api.covid19api.com/summary";
 
 button.addEventListener('click', getData())
 
-
+//get the data
 async function getData() {
   try{
   let data = await fetch(url)
+  //data in json
     let responce=await data.json();
     let count=responce.Countries[76]
     // console.log(responce.Countries[76])
@@ -19,11 +22,13 @@ async function getData() {
 <p>Total Recovery: ${count.TotalRecovered}</p>
 <p>Data: ${count.Date}</p>
 `;
+    //catch the error
   }catch(e){
     console.log(e)
   }
  
 }
+//call the function
 getData();
 
 
